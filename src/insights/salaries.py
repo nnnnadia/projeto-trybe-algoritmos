cdcf605dbba6b16a5006003a985a21400c718fd6
nnnnadia <tristaonadia@gmail.com>
2023-04-04 09:52:06 -0300

    <module> filter_by_salary_range: feat - return jobs that matches salary range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -101,4 +101,12 @@ def filter_by_salary_range(
     list
         Jobs whose salary range contains `salary`
     """
-    raise NotImplementedError
+    result = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                result.append(job)
+        finally:
+            continue
+
+    return result
