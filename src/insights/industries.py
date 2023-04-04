a73b3b0f0150e822502c1e2833688ff946b4efc4
nnnnadia <tristaonadia@gmail.com>
2023-04-04 08:32:03 -0300

    <module> get_unique_industries: get the unique values of the `industry` row

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,13 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    jobs = read(path)
+    unique_industries = []
+    for row in jobs:
+        industry = row["industry"]
+        if industry not in unique_industries:
+            unique_industries.append(industry)
+    return unique_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
