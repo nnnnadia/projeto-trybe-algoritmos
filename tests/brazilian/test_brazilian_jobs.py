06877db0c70e207f9ff7f1c020a5bea216484a07
nnnnadia <tristaonadia@gmail.com>
2023-04-04 11:06:31 -0300

    <test> test_brazilian_jobs: feat - test `read_brazilian_file`

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,17 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from unittest.mock import patch, mock_open
+
+
+mock_file_pt = [
+    {"salário": "2000", "título": "Maquinista", "tipo": "trainee"},
+]
+
+
+mock_file_en = [
+    {"salário": "2000", "título": "Maquinista", "tipo": "trainee"},
+]
 
 
 def test_brazilian_jobs():
-    pass
+    with patch("src.insights.jobs.read", mock_open(return_value=mock_file_pt)):
+        assert read_brazilian_file("some_path") == mock_file_en
