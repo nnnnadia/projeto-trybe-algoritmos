a47b6050ddab59b6ac3a133d73dd104e6eccc923
nnnnadia <tristaonadia@gmail.com>
2023-04-04 10:47:58 -0300

    <test> test_counter: feat - test `count_ocurrences`

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import patch, mock_open
 
 
 def test_counter():
-    pass
+    text = "a A a s f t w e f w y i f q F s e f g "
+    with patch("builtins.open", mock_open(read_data=text)):
+        assert count_ocurrences("some_path", word="f") == 5
